c41491a55b181064316554d3e869e9056c0c903b
oflops <oflops@gmail.com>
2016-03-21 14:53:32 +0200

    include babel-polyfill

--- src/artemis.core.js
+++ src/artemis.core.js
@@ -1,5 +1,11 @@
 'use strict';
+import "babel-polyfill";// must be first
+// import {Parser} from './parser/artemis-parser';
 
+/**
+ * Execute artemis query returning a list of element with rank
+ * @param query
+ */
 class Manager {
 	constructor(){
 		document.leerixFindElem = this.main;
@@ -7,13 +13,18 @@ class Manager {
 
 	main(query) {
 		// Parser str
+  //   	let parser = new Parser();
+		// let ast = parser.parse(query);
+        
+  //       console.log( JSON.stringify(ast,null,5) );
+
 		// Modeler elems
 		// Scorer DOM elems
 		// Marker DOM
 		document.querySelector(query).style.backgroundColor = "#98EB98";
 		document.querySelector(query).setAttribute("artemis-score", "1");
 		
-        console.log('DBG Manager.main ');
+        console.log('DBG Manager.main! ');
      
     }
 }
