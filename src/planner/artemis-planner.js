b3886a193cbbb9e5926da46e970df14b16f1a0d1
Uri Kalish <uri.kalish@hpe.com>
2016-04-06 21:04:28 +0300

    planner

--- src/planner/artemis-planner.js
+++ src/planner/artemis-planner.js
@@ -1,100 +1,91 @@
-export class Planner{
-    constructor(){
-        this.plans = document.settingsJSON.plans;
-        this.elemRegex = document.settingsJSON.phrases.find(x => x.location === "target-type").phrase;
-    }
+export class Planner {
 
-    /**
-     * This method find plan in settings and do json plans
-     * @param json from Parser
-     * @return json to Scorer with plans how to score elements from settings
-     * TODO: include analize "rel-location"
-     */
-    findPlan(term){
-       let plan =   this.plans.find(pln => pln.type === "elm-type" && pln.value === term);   
-       return plan ? plan.plan : null;    
-    }
-    isOneOfElements(term){
-      return new RegExp("(element|button|link|input|checkbox|radio|label|image|panel|toolbar|tab|dropdown|item)").test(term);
-    }
+  constructor(settings) {
+    this.settings = settings;
+    this.plans = settings.plans;
+  }
 
-    model(inputJson){
-        "use strict";
-        let jsonRes = [];
-        let jsonIncoming = JSON.parse(inputJson);
-       // console.log('INCOM: ',jsonIncoming)
-        let plan = {
-           "target": {
-            "and": []
-          }
-        };
-        
-        let getLastInPlan = function(){
-            if(!plan.target.and.length){
-                return null;
-            }
-            return plan.target.and[plan.target.and.length-1];
-        };
-        let isInsideRelation  = function(){
-          let last = getLastInPlan();
-          
-          if(last && last.scorer === 'target-relation'){
-              return  true;
-          }
+  get settings() { return this._settings; }
+  set settings(settings) { this._settings = settings; }
+  get plans() { return this._plans; }
+  set plans(plans) { this._plans = plans; }
 
-        };
-        let isRelation = function(word){              
-          return new RegExp("(above|below|left of|right of|inside)").test(word.replace('-',' '));
-        };
-        
-        jsonIncoming = jsonIncoming.map(d=>d.replace(/^-/,''));
-        
-    //     console.log('INCOM: ',jsonIncoming)
-        //jsonIncoming => ['button', 'left of', 'button', 'right of', 'button' ]
-        jsonIncoming.forEach((word)=>{
-            
-            
-            let relationType = word;
-            let relationPlan = {
-                    "scorer": "target-relation",
-                    "param": relationType,
-                    "weight": 1,
-                    "target": null
-            }; 
-            let freeTextPlan =  {
-                "scorer": "free-text",
-                "param": "",
-                "weight": 1
-            };   
-            let currPlan;       
-            if(this.isOneOfElements(word)){
-                
-                currPlan = this.findPlan(word);  
-                 
-                if(isInsideRelation()){//relation type
-                   getLastInPlan().target = currPlan;
-                }else{
-                   plan.target.and.push(currPlan); 
-                }
-            }else if(isRelation(word)){//new relation 
-                plan.target.and.push(relationPlan);
-            }else{
-                freeTextPlan.param = word;
-                currPlan =  freeTextPlan;
-                
-                if(isInsideRelation()){//relation type
-                   getLastInPlan().target = currPlan;
-                }else{
-                   plan.target.and.push(currPlan); 
-                }               
-            }
-        });
+  findPlan(term){
+     let plan = this.plans.find(pln => pln.type === "elm-type" && pln.value === term);
+     return plan ? plan.plan : null;
+  }
 
-        return JSON.stringify(plan, null, ' ');
-    }
-    
-    __model_node(json) {
-        return this.plans.find(x => x.type === json.type && x.value === json.value);
-        console.error("settings doesn't contain plan for this string:",json);
-    }
+  static isOneOfElements(term){
+    return new RegExp("(element|button|link|input|checkbox|radio|label|image|panel|toolbar|tab|dropdown|item)").test(term);
+  }
+
+  plan(modeledQuery) {
+    let jsonIncoming = modeledQuery;
+    let plan = {
+       "target": {
+        "and": []
+      }
+    };
+
+    let getLastInPlan = function() {
+      return plan.target.and.length ?
+        plan.target.and[plan.target.and.length-1] :
+        null;
+    };
+
+    let isInsideRelation = function() {
+      let last = getLastInPlan();
+      return (last && last.scorer === 'target-relation');
+    };
+
+    let isRelation = function(word) {
+      return new RegExp("(above|below|left of|right of|inside)").test(word.replace('-',' '));
+    };
+
+    jsonIncoming = jsonIncoming.map(d=>d.replace(/^-/,''));
+
+    //jsonIncoming => ['button', 'left of', 'button', 'right of', 'button' ]
+    jsonIncoming.forEach((word) => {
+      let relationPlan = {
+        "scorer": "target-relation",
+        "param": word,
+        "weight": 1,
+        "target": null
+      };
+      let freeTextPlan =  {
+        "scorer": "free-text",
+        "param": "",
+        "weight": 1
+      };
+      let currPlan;
+      if (Planner.isOneOfElements(word)) {
+        currPlan = this.findPlan(word);
+        if (isInsideRelation()) {
+          //relation type
+           getLastInPlan().target = currPlan;
+        } else {
+           plan.target.and.push(currPlan);
+        }
+      } else if (isRelation(word)) {
+        //new relation
+        plan.target.and.push(relationPlan);
+      } else {
+        freeTextPlan.param = word;
+        currPlan =  freeTextPlan;
+        if (isInsideRelation()) {
+          //relation type
+          getLastInPlan().target = currPlan;
+        } else {
+           plan.target.and.push(currPlan);
+        }
+      }
+    });
+
+    return JSON.stringify(plan, null, ' ');
+  }
+
+  __model_node(json) {
+      return this.plans.find(x => x.type === json.type && x.value === json.value);
+      console.error("settings doesn't contain plan for this string:",json);
+  }
 }
