68004ec8d6f02aa3a70987285644ba594056830e
Uri Kalish <uri.kalish@hpe.com>
2016-04-07 00:23:04 +0300

    misc changes

--- src/common/common-constants.js
+++ src/common/common-constants.js
@@ -1,6 +1,6 @@
 export const IGNORED_TAGS = ['script', 'noscript'];
-export const ARTEMIS_SCORE_ATTR = "artemis-score";
-export const ARTEMIS_CLASS = "artemis-mark-";
+export const ARTEMIS_SCORE_ATTR = 'artemis-score';
+export const ARTEMIS_CLASS = 'artemis-mark-';
 export const REL_LOCATION_TYPE = {
   RIGHT_OF: 'right-of',
   LEFT_OF:  'left-of',
