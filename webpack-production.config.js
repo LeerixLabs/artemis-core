4a5589a4919d990f2c7d85e74edb8cf2a1473db6
oflops <oflops@gmail.com>
2016-04-11 09:28:20 +0300

    Now can use "npm run build" for create minify core without map.

---
+++ webpack-production.config.js
@@ -0,0 +1,37 @@
+
+
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: { 
+        "artemis.core": './src/artemis.core.js'
+    },
+    output:{
+    
+        publicPath: '/',
+        filename: 'dist/[name].js',
+        library: "core"
+    },
+    debug:'true',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, 'src'),
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: "./src"
+    },
+    plugins:[
+        new webpack.optimize.UglifyJsPlugin({
+          compress: { warnings: false }
+        })
+    ]
+}
