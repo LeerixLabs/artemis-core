75b958cc5e2ec61a77e872759ff0dcb5bde79c5f
shootermv <shootermv@gmail.com>
2016-04-12 11:44:40 +0300

    parser tests added

--- test/parser_test.js
+++ test/parser_test.js
@@ -1,15 +1,21 @@
 import {Parser} from '../src/parser/artemis-parser.js'
 
+
+
 describe("Parse Test", () => {
+    let textParser = new Parser({});
+     
     "use strict";
-    let toJSON=function(json){
-       return JSON.stringify(json, null, ' ');
-    }
-    let parser = new Parser();
-    it("button should output value:'button'", () => {
-      expect(true).toBe(true);
+    it("'button' should be converted to [-button]", () => {
+         let words = textParser.parse("button");
+        expect(words).toEqual(['-button']);
+    }); 
+    it("'small button' should be converted to ['-small','-button']", () => {
+         let words = textParser.parse("small button");
+        expect(words).toEqual(['-small','-button']);
     });
-    it("small button should output value:'button' , value 'small'", () => {
-      expect(true).toBe(true);
-    });    
+    it("'button left of button' should be converted to [ '-button', '-left-of', '-button' ]", () => {
+         let words = textParser.parse("button left of button");
+        expect(words).toEqual([ '-button', '-left-of', '-button' ]);
+    });          
 });
