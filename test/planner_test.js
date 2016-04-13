f4f27f78246d51376d65f3e1de2200316574a229
shootermv <shootermv@gmail.com>
2016-04-13 11:30:06 +0300

    more planner tests

--- test/planner_test.js
+++ test/planner_test.js
@@ -11,5 +11,27 @@ describe("Planner Test: ", function(){
          let plan = planner.plan(parserOutput);
          expect(plan.target).toBeDefined();
     });
-
+  
+});
+describe("Planner Test: output of 'button':::", function(){
+    "use strict";
+ 
+    let planner = new Planner(settings);
+    let parserOutput = [{value:"button"}];
+    let plan = planner.plan(parserOutput);
+    let and = plan.target.and;
+    let or = and[0].or;
+    let firstMemeber = or[0];     
+     
+     
+    it("should have only 1 'and'", function(){        
+         expect(and.length).toBe(1);                  
+    });
+    it("should have 6 members inside 'or'", function(){
+        expect(or.length).toBe(6);        
+    });       
+    it("first member of 'or' shuold be html-element", function(){        
+        expect(firstMemeber.scorer).toBe('html-tag');
+        expect(firstMemeber.param).toBe('button');        
+    }); 
 });
