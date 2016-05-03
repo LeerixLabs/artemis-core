4317e036bf825db7ed99dd1d932dfab19ce209df
Uri Kalish <uri.kalish@hpe.com>
2016-05-03 20:24:15 +0300

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,29 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: { 
-        "artemis.core": './src/artemis.core.js'
-    },
-    output:{
-        publicPath: '/',
-        filename: 'dist/[name].js',
-        library: "artemisCore"
-    },
-    debug:'true',
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                include: path.join(__dirname, 'src'),
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    },
-    devServer: {
-        contentBase: "./src"
-    }
+  entry: {
+    'artemis.core': './src/artemis.core.js'
+  },
+  output:{
+    publicPath: '/',
+    filename: 'dist/[name].js',
+    library: 'artemisCore'
+  },
+  debug:'true',
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './src'
+  }
 };
