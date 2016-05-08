46425407bae5ae79984059bdaaa79d0e407fb7f5
Uri Kalish <uri.kalish@hpe.com>
2016-05-08 20:23:38 +0300

    add to json method

--- src/common/common-helper.js
+++ src/common/common-helper.js
@@ -1,21 +1,12 @@
 export class Helper {
 
-  ___LOG(msg) {
-    // html.console.log('ARTEMIS ' + msg);
-  }
-
-  ___DBG(msg) {
-    this.___LOG('DBG ' + msg);
-  }
-
-  ___WRN(msg) {
-    this.___LOG('WRN ' + msg);
+  static toJSON(object) {
+    return JSON.stringify(object, null, 4);
   }
 
   static isString(str1, str2){
       return typeof str1 == 'string' && typeof str2 == 'string' && str1.length &&  str2.length; 
   }
-
 
   static startsWith(str1, str2) {
     return this.isString(str1, str2) ? str1.indexOf(str2) === 0 : false;
@@ -47,12 +38,4 @@ export class Helper {
     return str.slice(1, str.length - 1);
   }
 
-  goHelp(displayAlert) {
-    var msg = 'Artemis' + '\n\n---------- Execution ----------\nImmediate: Shift+a | Delayed: Shift+s' + '\n\n---------- Ordinal ----------\n1st | 2nd | 3rd\n' + '\n\n---------- Target Adjectives ----------\nsmall | medium | large\n<free text>' + '\n\n---------- Target Type ----------\nelement | button | link | input | checkbox | label | image | dropdown | item | panel | toolbar' + '\n\n---------- Target Properties----------\nwith tag X\nwith class X\nwith style X:Y\nwith attribute X\nwith attribute value X\nwith attribute X equals to Y\nwith type X\nwith identity X\nwith text X\nat the top | at the bottom\non the left | on the right | on the middle' + '\n\n---------- Target-to-Target Relations ----------\nleft of | right of | above | below | near | inside <other target phrase>' + '\n\n---------- Examples ----------\nelement with tag div and class selected-tab\nsave button at the top\nlarge address input\nimage left of filter label' + '\n\n* For multiple non-reserved keywords, surround with single quotes / double quotes / tildes, or separate words by dashes e.g. \'save all\' button, or save-all button.';
-    this.___DBG(msg);
-    if (displayAlert) {
-      // html.window.alert(msg);
-      alert(msg);
-    }
-  }
 }
