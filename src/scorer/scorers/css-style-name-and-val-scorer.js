0860277d3c94afe8eb13a2735cc8a93f2acca2bc
Uri Kalish <uri.kalish@hpe.com>
2016-05-22 18:18:59 +0300

    style scorer

--- src/scorer/scorers/css-style-name-and-val-scorer.js
+++ src/scorer/scorers/css-style-name-and-val-scorer.js
@@ -5,10 +5,20 @@ export default class CssStyleNameAndValScorer {
   constructor(name, settings){
     this.name = name;
     this._settings = settings;
+    this._window = null;
   }
 
   score(elm, val) {
-    return 0;
+    //val should be an array of css style name and css style value
+    if (!elm || !elm.window || !val || val.length !== 2) {
+      return 0;
+    }
+    let score = 0;
+    let computedStyle = elm.window.getComputedStyle(elm.domElm);
+    if (computedStyle[val[0]] && computedStyle[val[0]] === val[1]) {
+      score = 1;
+    }
+    return score;
   }
 
 }
