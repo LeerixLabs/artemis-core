cb2ecfa673563b6b7ed4308fd85d729381f24edf
Uri Kalish <uri.kalish@hpe.com>
2017-02-14 20:03:56 +0200

    resolve gulp hangs after tests are done

--- gulp/gulp-test.js
+++ gulp/gulp-test.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var karma = require('karma');
 
-gulp.task('gulp-test', function (done) {
+function onTestsDone() {
+  process.exit();
+}
+
+gulp.task('gulp-test', function () {
   new karma.Server({
     configFile: __dirname + '/../karma.conf.js',
     singleRun: true
-  }, done).start();
+  }, onTestsDone).start();
 });
