b05b7be593196abe1287dc73de3725670f7ac6c4
Uri Kalish <uri.kalish@hpe.com>
2017-02-26 21:07:09 +0200

    logger

--- src/common/logger.js
+++ src/common/logger.js
@@ -1,45 +1,45 @@
 class Logger {
 
-  constructor() {
-    this._logLevelNames = ['ALL', 'DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL', 'OFF'];
-    this.setLogLevel('WARN');
-  }
+	constructor() {
+		this._logLevelNames = ['DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL', 'OFF'];
+		this.setLogLevel('WARN');
+	}
 
-  setLogLevel(levelName) {
-    if (levelName && (this._logLevelNames.indexOf(levelName) != -1)) {
-      this._logLevel = this._logLevelNames.indexOf(levelName);
-    }
-  }
+	setLogLevel(levelName) {
+		if (levelName && (this._logLevelNames.indexOf(levelName) !== -1)) {
+			this._logLevel = this._logLevelNames.indexOf(levelName);
+		}
+	}
 
-  _log(msg, levelName) {
-    if (this._logLevel <= this._logLevelNames.indexOf(levelName)) {
-      console.log('Artemis[' + levelName + '] ' + msg);
-    }
-  }
+	_log(msg, levelName) {
+		if (this._logLevel <= this._logLevelNames.indexOf(levelName)) {
+			console.log('Artemis[' + levelName + '] ' + msg);
+		}
+	}
 
-  trace(msg) {
-    this._log(msg, 'ALL');
-  };
+	isDebug() {
+		return this._logLevel === this._logLevelNames.indexOf('DEBUG');
+	}
 
-  debug(msg) {
-    this._log(msg, 'DEBUG');
-  };
+	debug(msg) {
+		this._log(msg, 'DEBUG');
+	};
 
-  info(msg) {
-    this._log(msg, 'INFO');
-  };
+	info(msg) {
+		this._log(msg, 'INFO');
+	};
 
-  warn(msg) {
-    this._log(msg, 'WARN');
-  };
+	warn(msg) {
+		this._log(msg, 'WARN');
+	};
 
-  error(msg) {
-    this._log(msg, 'ERROR');
-  };
+	error(msg) {
+		this._log(msg, 'ERROR');
+	};
 
-  fatal(msg) {
-    this._log(msg, 'FATAL');
-  };
+	fatal(msg) {
+		this._log(msg, 'FATAL');
+	};
 
 }
 
